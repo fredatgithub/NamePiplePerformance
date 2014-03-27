f9907ddd6e52da5f6c23d5b2f5217ad6f17ce29d
Faisal Mansoor <faisal.mansoor@gmail.com>
2014-03-27 11:41:32 -0400

    initial checkin

---
+++ NamedPipeClient/Client.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO.Pipes;
+using System.Threading;
+
+namespace NamedPipeClient
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            string pipeName = args[0];
+            int bufferSize = int.Parse(args[1]);
+
+            Console.WriteLine("Connecting to pipe {0} [bufferSize: {1}]", pipeName, bufferSize);
+
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            var client = new NamedPipeClientStream(pipeName);
+            
+            while (!client.IsConnected)
+            {
+                Console.WriteLine("Attempting to connect to {0}", pipeName);
+
+                client.Connect((int) TimeSpan.FromSeconds(1).TotalMilliseconds);
+            }
+
+            Console.WriteLine("Connected");
+            
+            var buffer = new byte[bufferSize];
+
+            var startTime = DateTime.Now;
+            long totalReads = 0;
+            while (client.IsConnected)
+            {
+                client.Read(buffer, 0, buffer.Length);
+                totalReads++;
+
+                if (totalReads%1000 == 0)
+                {
+                    double totalBytes = totalReads*bufferSize;
+                    double totalMBps = (totalBytes / (1024*1024) ) / (DateTime.Now - startTime).TotalSeconds ;
+                    Console.WriteLine("Rate = {0:F1} mb/s", totalMBps);
+                }
+            }
+
+            Console.WriteLine("Disconnected.");
+        }
+    }
+}
---
+++ NamedPipeServer/Server.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO.Pipes;
+using System.Linq;
+using System.Threading;
+
+namespace NamedPipeServer
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            const string pipeName = "testnp";
+
+            if (args.Length == 0)
+            {
+                args = new []{"1024"};
+            }
+
+            int bufferSize = int.Parse(args[0]);
+
+            var buffer = Enumerable.Range(0, bufferSize).Select(i => (byte)i).ToArray();
+
+            //Console.WriteLine(String.Join(", ", Enumerable.Range(0, bufferSize)));
+
+            Console.WriteLine("Listening..");
+            //var server = new NamedPipeServerStream(pipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Message);
+            var server = new NamedPipeServerStream(pipeName);
+            
+
+            Process.Start("NamedPipeClient.exe", string.Format("{0} {1}", pipeName, bufferSize));
+            
+            server.WaitForConnection();
+
+            while (!Console.KeyAvailable)
+            {
+                server.Write(buffer, 0, buffer.Length);
+                //Thread.Sleep(TimeSpan.FromMilliseconds(5));
+            }
+
+            Console.WriteLine("Done");
+        }
+    }
+}
